dbeb0aef9efc405b7533118ca47af7487fe7f8b2
hako1912 <4769754+hako1912@users.noreply.github.com>
2019-06-16 11:12:44 +0000

    clean

--- webpack.common.js
+++ webpack.common.js
@@ -6,35 +6,8 @@ module.exports = {
     extensions: [".ts", ".tsx", ".js", "jsx", "scss"],
     plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })]
   },
-  optimization: {
-    minimize: false // <---- disables uglify.
-    // minimizer: [new UglifyJsPlugin()] if you want to customize it.
-  },
   module: {
     rules: [
-      // {
-      //   test: /\.(jsx?)$/,
-      //   exclude: /node_modules/,
-      //   use: [
-      //     {
-      //       loader: "babel-loader",
-      //       options: {
-      //         presets: [
-      //           ["@babel/react"],
-      //           [
-      //             "@babel/env",
-      //             {
-      //               useBuiltIns: "usage",
-      //               corejs: 3,
-      //               modules: false
-      //             }
-      //           ]
-      //         ],
-      //         cacheDirectory: true
-      //       }
-      //     }
-      //   ]
-      // },
       {
         test: /\.tsx?$/,
         loader: "ts-loader",
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,18 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
-const PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin')
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const PeerDepsExternalsPlugin = require("peer-deps-externals-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
-  entry: './src/index.ts',
+  mode: "production",
+  entry: "./src/index.ts",
   output: {
-    path: __dirname + '/dist',
-    filename: 'index.js',
-    library: 'mylib2',
-    libraryTarget: 'umd',
+    path: __dirname + "/dist",
+    filename: "index.js",
+    library: "mylib2",
+    libraryTarget: "umd",
     umdNamedDefine: true,
     // https://medium.com/@JakeXiao/window-is-undefined-in-umd-library-output-for-webpack4-858af1b881df
     globalObject: "this" //!!!This line
   },
-  plugins: [new PeerDepsExternalsPlugin()],
-})
+  plugins: [new PeerDepsExternalsPlugin()]
+});
